827c9c3d608d94597a76b15ddfd72f30052a4660
IBRAHIM SULU-GAMBARI <159025575+Ibrahimsulu@users.noreply.github.com>
2025-05-04 19:48:53 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -274,6 +274,5 @@ def search():
 
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT isn't set
-    app.run(debug=True, host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
